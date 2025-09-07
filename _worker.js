ed985e0151430a6895e31e7ade79402a0c8a9d85
Lenagitpus <githubrepo131@gmail.com>
2025-09-07 14:57:07 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from "cloudflare:sockets";
 
 // Variables
 const rootDomain = "ahem7553.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "user"; // Ganti dengan nama workers kalian
+const serviceName = "vc2"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
